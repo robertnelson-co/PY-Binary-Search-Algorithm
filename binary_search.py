190eaf2618ba68d254779dd7ff858954ce075e73
robertnelson-co <hello@robertnelson.co>
2023-11-01 21:10:35 -0400

    writing baseline for binary search algorithm

--- binary_search.py
+++ binary_search.py
@@ -1,2 +1,12 @@
 # code your iterative algorithm here
 
+def binary_search(data, el):
+    first = 0
+    last = len(data)-1
+    found = False
+
+    while first <= last and not found:
+        mid = (first+last)//2
+
+        if data[mid] == el:
+            found == True
